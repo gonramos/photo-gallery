9c4445426c5549388ad05b2984f4809117237906
gonramos <goncalo.rms@gmail.com>
2020-10-12 12:17:44 +0200

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-## Create a gallery of your favourite photos online
-### Add link to image, description and comments
-
-Using
-* React
-* React-Redux
-* React-Router
-* Redux-Thunk
-* Firebase
---
+++ precache-manifest.aa52b8f1b41efd9a5cb428e0e4e5121b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae55644b5e25220944948fbe5fadbe08",
+    "url": "/photo-gallery/index.html"
+  },
+  {
+    "revision": "da5a9265b8a8d187d44a",
+    "url": "/photo-gallery/static/css/main.dc3f0505.chunk.css"
+  },
+  {
+    "revision": "7e2a8c9ad29a9a47963e",
+    "url": "/photo-gallery/static/js/2.2e8c2805.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/photo-gallery/static/js/2.2e8c2805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da5a9265b8a8d187d44a",
+    "url": "/photo-gallery/static/js/main.9abc977f.chunk.js"
+  },
+  {
+    "revision": "8721460b5d8e9736266d",
+    "url": "/photo-gallery/static/js/runtime-main.6f17d3b3.js"
+  },
+  {
+    "revision": "f6ea751e936ade6edcd03a26b8153b4a",
+    "url": "/photo-gallery/static/media/Poppins-Light.f6ea751e.ttf"
+  }
+]);
