ba81fcb87bd417aa6c80d0ead5ecd46e02acfd4c
gonramos <goncalo.rms@gmail.com>
2021-11-09 16:01:39 +0100

    Add title to image

--- src/components/Photowall.js
+++ src/components/Photowall.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import Photo from './Photo'
 import PropTypes from 'prop-types'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Photowall = props => {
   return (
     <div>
-      <Link className="addIcon" to="/AddPhoto"> </Link>
+      <Link className="addIcon" title="Add Photo" to="/AddPhoto"> </Link>
       <div className="photoGrid">
         {props.posts
-          .sort((x,y) => {return y.id - x.id})
+          .sort((x, y) => { return y.id - x.id })
           .map((post, index) =>
-          <Photo key={index} post={post} {...props} index={index} />
-        )}
+            <Photo key={index} post={post} {...props} index={index} />
+          )}
       </div>
     </div>
   )
